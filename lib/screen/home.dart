7b598da311a457689402e8135b3174a22a43a394
Md Tahmid Mozaffar <tahmid1989@gmail.com>
2022-10-16 04:52:43 -0400

    implementation category listview in home screen

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -10,6 +10,43 @@ class HomeScreen extends StatefulWidget {
 class _HomeState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: InkWell(onTap: () {}, child: Text("Home")));
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Text("Expense Category"),
+              Icon(Icons.add_circle),
+            ],
+          ),
+          Container(
+            color: Colors.deepPurpleAccent,
+            height: 40,
+            child: ListView(scrollDirection: Axis.horizontal, children: [
+              Text("One"),
+              Text("One"),
+              Text("One"),
+              Text("One"),
+              Text("One"),
+              Text("One"),
+              Text("One"),
+              Text("One"),
+              Text("One"),
+              Text("One"),
+              Text("One"),
+              Text("One"),
+              Text("One"),
+              Text("One"),
+              Text("One"),
+              Text("One"),
+              Text("One"),
+              Text("One"),
+            ],),
+          )
+        ],
+      ),
+    );
   }
 }
